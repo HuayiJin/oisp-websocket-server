35270642a2561d1e49ce4df8ad5e2a31a3783996
HuayiJin <keplar_gd@icloud.com>
2018-03-05 15:21:22 +0800

    test of server.js

--- test/serverTests.js
+++ test/serverTests.js
@@ -1,5 +1,4 @@
-/*
-Copyright (c) 2018, Intel Corporation
+copyright (c) 2018, Intel Corporation
 
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
@@ -21,3 +20,55 @@ ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
+
+var assert = require('chai').assert,
+	sinon = require('sinon'),
+	rewire = require('rewire');
+
+var sinonTestFactory = require('sinon-test');
+var sinonTest = sinonTestFactory(sinon);
+
+var fileToTest = "../server.js";
+
+var fakelogger = {
+	error: function(err) {
+		return err;
+	}
+};
+var fakecert = "";
+var fakedecode = {
+	sub: "deviceId"
+};
+
+
+
+describe(fileToTest, function() {
+	var toTest = rewire(fileToTest);
+
+	it('Shall get callback once when authorize device', sinonTest(function(done) {
+		toTest.__set__("cert", fakecert);
+		toTest.__set__("logger", fakelogger);
+
+		var callbackSpy = this.spy();
+		var authorizeDevice = toTest.__get__("authorizeDevice");
+		authorizeDevice("Token", "deviceId", callbackSpy)
+		assert(callbackSpy.calledOnce);
+		done();
+	}));
+
+	it('Shall parse Message', sinonTest(function(done) {
+		var callbackSpy = this.spy();
+		var parseMessage = toTest.__get__("parseMessage");
+		var msg = '{"hello":"world"}';
+		parseMessage(msg, callbackSpy);
+		assert(callbackSpy.calledOnce);
+		done();
+	}));
+
+	it('Shall build actuation', sinonTest(function(done) {
+		var content = {hello:'world'};
+		var buildActuation = toTest.__get__("buildActuation");
+		assert.equal(buildActuation(content), '{"code":1024,"content":{"hello":"world"}}', "get build");
+		done();
+	}));
+});
